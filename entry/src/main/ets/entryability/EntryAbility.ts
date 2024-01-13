2ad9c675c7782bd91206ecfb06ffe8ff93691004
dinglong <dillonl49@163.com>
2024-01-14 00:28:12 +0800

    chore:#封装请求

--- entry/src/main/ets/entryability/EntryAbility.ts
+++ entry/src/main/ets/entryability/EntryAbility.ts
@@ -4,57 +4,27 @@ import window from '@ohos.window';
 import type { Permissions } from '@ohos.abilityAccessCtrl'
 import abilityAccessCtrl from '@ohos.abilityAccessCtrl'
 const PERMISSIONS: Array<Permissions> = ['ohos.permission.INTERNET', 'ohos.permission.SET_WALLPAPER']
-
 export default class EntryAbility extends UIAbility {
   onCreate(want, launchParam) {
-    // let atManager: abilityAccessCtrl.AtManager = abilityAccessCtrl.createAtManager()
-    // try {
-    //   atManager.requestPermissionsFromUser(this.context, PERMISSIONS).then((data) => {
-    //     AppStorage.SetOrCreate('filePath', this.context.filesDir)
-    //     AppStorage.SetOrCreate('context', this.context)
-    //     console.info('appstart'+'MainAbility onCreate')
-    //
-    //   }).catch((err) => {
-    //     console.info('appstart'+`err: ${JSON.stringify(err)}`)
-    //
-    //   })
-    // } catch (err) {
-    //   console.info('appstart'+ `catch err->${JSON.stringify(err)}`);
-    // }
   }
 
   onDestroy() {
-    // hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onDestroy');
   }
 
   onWindowStageCreate(windowStage: window.WindowStage) {
     let context = this.context;
     let pathDir = context.filesDir;
-    console.error("文件路径："+pathDir)
-    // Main window is created, set main page for this ability
-    // hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
-
     windowStage.loadContent('pages/Sign', (err, data) => {
       if (err.code) {
-        // hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
         return;
       }
-      // hilog.info(0x0000, 'testTag', 'Succeeded in loading the content. Data: %{public}s', JSON.stringify(data) ?? '');
     });
   }
 
-  onWindowStageDestroy() {
-    // Main window is destroyed, release UI related resources
-    // hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageDestroy');
-  }
+  onWindowStageDestroy() {}
 
-  onForeground() {
-    // Ability has brought to foreground
-    // hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onForeground');
-  }
+  onForeground() {}
 
   onBackground() {
-    // Ability has back to background
-    // hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onBackground');
   }
 }
--- entry/src/main/ets/interface/index.ts
+++ entry/src/main/ets/interface/index.ts
@@ -2,3 +2,13 @@ export interface OpearInter{
   icon: unknown,
   title: string
 }
+
+export interface SignInter{
+  uname:string,
+  password:string
+}
+export interface SignResInter{
+  code?:number,
+  data?:string,
+  msg?:string
+}
