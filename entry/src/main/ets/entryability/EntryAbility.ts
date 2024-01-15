1f6d3fcd162ec0a2227e8700b60df1f5bc8c2056
dinglong <dillonl49@163.com>
2024-01-15 23:43:33 +0800

    feat:#增加导航组件

--- entry/src/main/ets/entryability/EntryAbility.ts
+++ entry/src/main/ets/entryability/EntryAbility.ts
@@ -1,16 +1,8 @@
 import UIAbility from '@ohos.app.ability.UIAbility';
-import hilog from '@ohos.hilog';
 import window from '@ohos.window';
-import type { Permissions } from '@ohos.abilityAccessCtrl'
-import abilityAccessCtrl from '@ohos.abilityAccessCtrl'
-const PERMISSIONS: Array<Permissions> = ['ohos.permission.INTERNET', 'ohos.permission.SET_WALLPAPER']
 export default class EntryAbility extends UIAbility {
-  onCreate(want, launchParam) {
-  }
-
-  onDestroy() {
-  }
-
+  onCreate(want, launchParam) { }
+  onDestroy() {}
   onWindowStageCreate(windowStage: window.WindowStage) {
     let context = this.context;
     let pathDir = context.filesDir;
@@ -19,6 +11,13 @@ export default class EntryAbility extends UIAbility {
         return;
       }
     });
+    windowStage.getMainWindow((err, winclass) => {
+      if(err.code){
+        return
+      }
+      let av = winclass.getWindowAvoidArea(window.AvoidAreaType.TYPE_SYSTEM);
+      let rect = av.topRect
+    })
   }
 
   onWindowStageDestroy() {}
