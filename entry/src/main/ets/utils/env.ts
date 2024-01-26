352b2c12d28429c9c07a942d23629ddf5f93f086
dinglong <dillonl49@163.com>
2024-01-26 22:58:50 +0800

    feat:🎗️壁纸分类分页加载完成[page]

--- entry/src/main/ets/utils/env.ts
+++ entry/src/main/ets/utils/env.ts
@@ -1,2 +1,2 @@
-// export const base_url = "http://192.168.2.102"
-export const base_url = "https://www.dillonl.com/wapi"
+export const base_url = "http://172.20.10.2"
+// export const base_url = "https://www.dillonl.com/wapi"
